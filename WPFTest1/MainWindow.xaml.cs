e35585972ab497ccc9674d2d9ecfe34d90653139
darodrgu <40703140+darodrgu@users.noreply.github.com>
2019-03-19 11:40:46 -0600

    First commit, initial project with tests on WPF forms

---
+++ WPFTest1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTest1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Top = 0;
+            this.Left = System.Windows.SystemParameters.PrimaryScreenWidth - 600;
+            this.ResizeMode = ResizeMode.NoResize;
+            cbxProcess.ItemsSource = new string[] {"Password Reset","Elevation","Option 3"};
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            Window window = (Window)sender;
+            window.Topmost = true;
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            this.Width = 600;
+            this.Height = 450;
+            this.Topmost = true;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.WindowState = WindowState.Minimized;
+
+        }
+    }
+}
